b9c14b55e415e3cfc85c2ad332b2ab77878c4e37
shivakeshav1626 <goudshivakeshav@gmail.com>
2025-03-10 10:42:16 +0530

    done

---
+++ backend/controller/product.js
@@ -0,0 +1,24 @@
+router.get('/cartProduct', async (req, res) => {
+    const { email } = req.query;
+    try {
+        if (!email) 
+            return res.status(403).send('login to add to cart');
+
+        const user = await User.findOne({ email }).populate({ 
+            path: 'cart.productId', 
+            model: 'Product' 
+        });
+
+        if (!user) 
+            return res.status(400).send('register to add to cart');
+
+        res.status(200).json({
+            message: 'Cart retrieved successfully',
+            cart: user.cart
+        });
+
+    } catch (err) {
+        console.error('Server error:', err);
+        res.status(500).json({ error: 'Server Error' });
+    }
+});
